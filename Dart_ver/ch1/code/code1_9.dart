e464ea3baf638ad3eb9b2db7efb8acf5d59fe9a7
hide0621 <fujihara_hideyuki@retail-ai.jp>
2025-02-07 11:09:28 +0900

    return switch文を書いてみた

--- Dart_ver/ch1/code/code1_9.dart
+++ Dart_ver/ch1/code/code1_9.dart
@@ -59,6 +59,54 @@ class Coodinate {
 //   const DisplayEnvironment(this.pixelPerInch);
 // }
 
+/// バッドコードに繋がっていく...
+enum UnitType {
+  PIXEL,
+  POINT,
+}
+
+/// バッドコードな座標クラス
+///
+/// switch文の別の表現方法
+class Coodinate2 {
+  final double x;
+  final double y;
+  final UnitType unitType;
+
+  const Coodinate2(this.x, this.y, this.unitType);
+
+  static const int POINTS_PER_INCH = 72;
+
+  Coodinate2 plus(Coodinate2 that, DisplayEnvironment environment,
+      UnitType resultUnitType) {
+    final newX = _convertType(x, unitType, resultUnitType, environment) +
+        _convertType(that.x, that.unitType, resultUnitType, environment);
+    final newY = _convertType(y, unitType, resultUnitType, environment) +
+        _convertType(that.y, that.unitType, resultUnitType, environment);
+    return Coodinate2(newX, newY, resultUnitType);
+  }
+
+  double _convertType(double value, UnitType from, UnitType to,
+      DisplayEnvironment environment) {
+    if (from == to) {
+      return value;
+    }
+
+    // switch文の別の表現方法
+    // こっちの方が読みやすいし、スッキリしている
+    return switch (to) {
+      UnitType.PIXEL => value * environment.pixelPerInch / POINTS_PER_INCH,
+      UnitType.POINT => value * POINTS_PER_INCH / environment.pixelPerInch,
+    };
+  }
+}
+
+class DisplayEnvironment {
+  final int pixelPerInch;
+
+  const DisplayEnvironment(this.pixelPerInch);
+}
+
 void code1_9() {
   print('code1_9');
 }
