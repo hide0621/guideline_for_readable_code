1f70050d18203a8348d66303fe629495eb8b6d9c
hide0621 <fujihara_hideyuki@retail-ai.jp>
2025-02-19 20:24:19 +0900

    KISSの原則についてコーディングしてみた

---
+++ Dart_ver/ch1/code/code1_12.dart
@@ -0,0 +1,33 @@
+/**
+ * ただし、以下のコードはpubspec.yamlにrxdartの依存関係を追加する必要がある
+ */
+// import 'package:rxdart/rxdart.dart';
+
+
+// 大元のコード
+// Single<List<int>> getActualDataSingle() {
+//   return Single.fromCallable(() => dataProvider.provide())
+//       .subscribeOn(Scheduler.io);
+// }
+
+
+// KISSを踏まえたコード
+// Single<List<int>> getStubDataSingle() => Single.just([1, 10, 100]);
+
+
+// KISSではないコード
+// Single<List<int>> getStubDataSingle() {
+//   return Single.fromCallable(() => [1, 10, 100])
+//       .subscribeOn(Scheduler.io);
+// }
+
+
+// KISSではないコードをより極端にしたもの
+// Single<List<int>> getStubDataSingle() {
+//   return Observable.range(1, 2)
+//       .reduce<List<int>>(
+//         [1],
+//         (List<int> list, int _) => [...list, list.last * 10],
+//       )
+//       .subscribeOn(Scheduler.io);
+// }
