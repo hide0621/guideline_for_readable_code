398925cd8444381ae6b25afa6a2444cc906888da
hide0621 <fujihara_hideyuki@retail-ai.jp>
2025-03-12 18:03:20 +0900

    単一責任の原則に則っていないケースを一つ書いてみた

---
+++ Dart_ver/ch1/code/code1_15.dart
@@ -0,0 +1,20 @@
+// class Alviss {
+//   // テキスト表示、デバイスの破棄、ロケットの打ち上げ、その他諸々を行うかもれない...
+//   void doEverything(UniverseState state) {
+//     switch (state) {
+//       case UniverseState.Creation:
+//         // 宇宙の創造
+//         break;
+//       case UniverseState.Destruction:
+//         // 宇宙の破壊
+//         break;
+//       case UniverseState.RocketLaunch:
+//         // ロケットの打ち上げ
+//         break;
+//     }
+//   }
+// }
+
+void code1_15() {
+  print('code1_15');
+}
