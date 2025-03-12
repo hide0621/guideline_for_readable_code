46f1139ec369ab276881e485921d71b61ea0370d
hide0621 <fujihara_hideyuki@retail-ai.jp>
2025-03-12 18:33:47 +0900

    単一責任の原則に違反しているケースをもう一つ書いてみた

--- Dart_ver/ch1/code/code1_15.dart
+++ Dart_ver/ch1/code/code1_15.dart
@@ -16,6 +16,27 @@
 //   }
 // }
 
+/// 単一責任の原則に則っていない例
+// class LibraryBookRentalData {
+//   // 書籍情報。IDとタイトルのリスト
+//   final List<BookId> bookIds;
+//   final List<String> bookNames;
+
+//   // 書籍の貸し出し情報。利用者の名前と返却期限のマップ
+//   final Map<BookId, String> bookIdToRenterNameMap;
+//   final Map<BookId, DateTime> bookIdToDueDateMap;
+
+//   LibraryBookRentalData({
+//     required this.bookIds,
+//     required this.bookNames,
+//     required this.bookIdToRenterNameMap,
+//     required this.bookIdToDueDateMap,
+//   });
+
+//   String? findRenterName(String? bookName) {...}
+//   DateTime? findDueDate(String? bookName) {...}
+// }
+
 void code1_15() {
   print('code1_15');
 }
