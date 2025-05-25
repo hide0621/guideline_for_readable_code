d612c23a64562bba83da1fc24ac29a5bb81286c4
hide0621 <fujihara_hideyuki@retail-ai.jp>
2025-05-26 08:35:41 +0900

    feat: 5章のセットアップ

---
+++ Dart_ver/ch5/ch5.dart
@@ -0,0 +1,3 @@
+void ch5() {
+  print("Hello, Chapter 5!");
+}
--- Dart_ver/main.dart
+++ Dart_ver/main.dart
@@ -1,5 +1,7 @@
-import 'ch1/ch1.dart';
+// import 'ch1/ch1.dart';
+import 'ch5/ch5.dart';
 
 void main() {
-  ch1();
+  // ch1();
+  ch5();
 }
