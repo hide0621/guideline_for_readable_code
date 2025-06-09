9038947bf00e1ab56328537492b8eb9367d98e5c
hide0621 <fujihara_hideyuki@retail-ai.jp>
2025-06-09 17:17:56 +0900

    feat: 関数の実引数が二重のインラインメソッドになっている関数をローカル変数の抽出で解消してみた

--- Dart_ver/ch5/code/ch5_11.dart
+++ Dart_ver/ch5/code/ch5_11.dart
@@ -1,6 +1,13 @@
-// バッドコード
+/// バッドコード
 // showDialogOnError(
 //   presenter.updateSelfProfileView(
 //     repository.queryUserModel(userId)
 //     )
 // );
+
+/// グッドコード ~ 実引数のネストをローカル関数で解消 ~
+// final userModel = repository.queryUserModel(userId);
+
+// final viewUpdateResult = presenter.updateSelfProfileView(userModel);
+
+// showDialogOnError(viewUpdateResult);
